71c1500194822e50c8ad136bb88c39a04172ec49
byeolhaha <j35635aa@gmail.com>
2024-03-17 22:13:07 +0900

    refactor : chat message 도큐먼트로 저장되도록 수정

--- chat/src/main/java/com/garden/back/garden/domain/GardenChatMessage.java
+++ chat/src/main/java/com/garden/back/garden/domain/GardenChatMessage.java
@@ -5,54 +5,53 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "garden_chat_messages")
-@Entity
+@Document(collection = "garden_chat_messages")
 public class GardenChatMessage {
 
     protected GardenChatMessage() {
     }
 
     @Id
-    @Column(name = "chat_message_id", nullable = false)
     private Long chatMessageId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "chat_room_id", nullable = false)
-    private GardenChatRoom chatRoom;
+    @Field(name = "chat_room_id")
+    private Long chatRoomId;
 
-    @Column(name = "member_id", nullable = false)
+    @Field(name = "member_id")
     private Long memberId;
 
-    @Column(name = "chat_contents", columnDefinition = "TEXT")
+    @Field(name = "chat_contents")
     private String contents;
 
-    @Column(name = "read_or_not", nullable = false)
+    @Field(name = "read_or_not")
     private boolean readOrNot;
 
     @CreatedDate
-    @Column(name = "created_at", updatable = false, nullable = false)
+    @Field(name = "created_at")
     private LocalDateTime createdAt;
 
     private GardenChatMessage(
         Long chatMessageId,
-        GardenChatRoom chatRoom,
+        Long chatRoomId,
         Long memberId,
         String contents,
         boolean readOrNot
     ) {
         Assert.notNull(chatMessageId, "chatMessageId는 null일 수 없습니다.");
-        Assert.notNull(chatRoom, "Chat Room은 null일 수 없습니다.");
+        Assert.notNull(chatRoomId, "Chat Room은 null일 수 없습니다.");
         Assert.isTrue(memberId > 0, "유저 아이디는 0이거나 0보다 작을 수 없습니다.");
         Assert.notNull(contents, "메세지 내용은 null일 수 없습니다.");
 
         this.chatMessageId = chatMessageId;
-        this.chatRoom = chatRoom;
+        this.chatRoomId = chatRoomId;
         this.memberId = memberId;
         this.contents = contents;
         this.readOrNot = readOrNot;
@@ -60,14 +59,14 @@ public class GardenChatMessage {
 
     public static GardenChatMessage of(
         Long chatMessageId,
-        GardenChatRoom chatRoom,
+        Long chatRoomId,
         Long memberId,
         String contents,
         boolean readOrNot
     ) {
         return new GardenChatMessage(
             chatMessageId,
-            chatRoom,
+            chatRoomId,
             memberId,
             contents,
             readOrNot
@@ -81,7 +80,7 @@ public class GardenChatMessage {
     ) {
         return new GardenChatMessage(
             chatMessageId,
-            GardenChatRoom.of(gardenChatMessageDomainParam.roomId()),
+            gardenChatMessageDomainParam.roomId(),
             gardenChatMessageDomainParam.memberId(),
             gardenChatMessageDomainParam.contents(),
             true
@@ -94,11 +93,16 @@ public class GardenChatMessage {
     ) {
         return new GardenChatMessage(
             chatMessageId,
-            GardenChatRoom.of(gardenChatMessageDomainParam.roomId()),
+            gardenChatMessageDomainParam.roomId(),
             gardenChatMessageDomainParam.memberId(),
             gardenChatMessageDomainParam.contents(),
             false
         );
+    }
+
+    public GardenChatMessage readMessage() {
+        this.readOrNot = true;
+        return this;
     }
 
 }
